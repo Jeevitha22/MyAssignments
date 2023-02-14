ffa1c82639285dde68297697dc121f023eeb84e7
Jeevitha22 <124479398+Jeevitha22@users.noreply.github.com>
2023-02-14 16:40:44 +0530

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,19 @@
+package week1.day2;
+
+public class FibonacciSeries {
+public static void main(String[] args) {
+	int a, b,c;
+	a=0;
+	b=1;
+	//c=0;
+	
+	for(int i=1;i<=11;++i)
+	{
+		System.out.println(a+"");	
+	c=a+b;
+	a=b;
+	b=c;
+	//System.out.println(c);
+	}	
+}
+}
